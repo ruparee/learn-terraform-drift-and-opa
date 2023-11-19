9007aca4c82346c8e0934ecc523cf11dc281e646
ruparee <eruparee@me.com>
2023-11-19 21:55:36 +0000

    Updated files

--- variables.tf
+++ variables.tf
@@ -8,4 +8,20 @@ variable "bastion_instance_type" {
   type        = string
 }
 
+# variable "AWS_SECRET_ACCESS_KEY" {
+#   description = "AWS secret access key"
+# }
+
+# variable "AWS_ACCESS_KEY_ID" {
+#   description = "AWS access key ID"
+# }
+
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
+}
+
+variable "aws_access_key_id" {
+  description = "AWS access key ID"
+}
+
 
