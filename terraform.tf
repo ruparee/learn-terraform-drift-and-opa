9485bbb2f49f8666b9a1b0fa82727e434fb9ad67
ruparee <eruparee@me.com>
2023-11-20 00:54:34 +0000

    Update OPA policy

--- terraform.tf
+++ terraform.tf
@@ -8,9 +8,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.10.0"
+      version = "=> 4.10.0"
     }
   }
 
-  required_version = "~> 1.4.0"
+  required_version = "=> 1.4.0"
 }
